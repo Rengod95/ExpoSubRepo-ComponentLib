f0815b0e5caa486b89e40177acb56095c2faef52
Rengod95 <aorwn970@gmail.com>
2024-09-24 23:58:52 +0900

    환경변수 변경 작업

--- BootStrap/configureExternalAuthValidator.ts
+++ BootStrap/configureExternalAuthValidator.ts
@@ -1,7 +1,7 @@
 import {AuthService, Validator} from '@/Service/lib/Auth/Adapter.ts';
 
 export const configureExternalAuthValidator = (validator: Validator) => {
-  if (AuthService.getInstance().isServiceActivated && AuthService.getInstance().useExternalValidator) {
+  if (AuthService.getInstance().isServiceActivated !== 'none' && AuthService.getInstance().useExternalValidator) {
     AuthService.getInstance().initValidator(validator);
   }
 };
